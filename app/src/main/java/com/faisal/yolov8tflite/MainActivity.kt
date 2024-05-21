42e54bfbab6051e2e8ceb5f3e56db5e950d9866a
Faisal-style <faisalrezarahmat1@gmail.com>
2024-05-21 10:56:50 +0700

    testing iqlab activity

--- app/src/main/java/com/faisal/yolov8tflite/MainActivity.kt
+++ app/src/main/java/com/faisal/yolov8tflite/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.faisal.yolov8tflite.Constants.LABELS_PATH
 import com.faisal.yolov8tflite.Constants.MODEL_PATH
+import com.faisal.yolov8tflite.databinding.ActivityIdghomMisliBinding
 import com.faisal.yolov8tflite.databinding.ActivityMainBinding
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
